9cd2c7cd50d257ad67e6be084d57d8a3de3ac428
Natalitta <nataliialebedieva@gmail.com>
2023-03-22 22:10:17 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,76 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {"robot": 0, "user": 0}
+
+class Board:
+    """
+    Creates the board, its size, type (robot or user's board).
+    Player's name.
+    """
+    def __init__(self, size, ships_number, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.ships_number = ships_number
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))
+
+    def guess(self, x, y):
+        self.guesses.append((x, y))
+        self.board[x][y] = "x"
+
+        if (x, y) in self.ships:
+            self.board[x][y] ="*"
+            return "yes"
+        else:
+            return "no"
+
+        
+    def add_ship(self, x, y, type="robot"):
+        if len(self.ships) >= self.ships_number:
+            print("Sorry, no more ships can be added")
+        else:
+            self.ships.append((x, y))
+            if self.type == "user":
+                self.board[x][y] = "&"
+
+
+def random(size):
+    """
+    It helps to return a random number (0 - size)
+    """
+    return radint (0, size - 1)
+
+
+def validate(x, y, board):
+
+
+def populate_board(board):
+
+
+def make_guess(board):
+
+
+def play_game(robot_board, user_board):
+
+
+def new_game:
+    """
+    It begins the game from the start. 
+    New boards, zero score.
+    """
+    size = 6
+    ships_number = 5
+    scores["robot"] = 0
+    scores["user"] = 0
+    print("-" * 35)
+    print("Welcome to the Battleships game!")
+    print(f"Your board size: {size}. You have {ships_number} ships.")
+    print("Coordinates start in the left top corner (row: 0, column: 0)")
+
+
